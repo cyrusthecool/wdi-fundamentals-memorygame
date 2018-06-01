34c07bf0a1ae1558e1516c3b6e00b3ceeec083e5
Kourosh Azarbayjani <kouroshazarbayjani@Kouroshs-Air.gateway>
2018-06-01 13:25:25 +1000

    create game logic

--- memory-game/JS/main.js
+++ memory-game/JS/main.js
@@ -1,7 +1,17 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped" +" "+cardOne);
-console.log("User flipped" +" "+cardTwo);
+console.log("User flipped " + cardOne);
+var cards = ['queen','queen','king','king'];
+var cardsInPlay=[];
+var cardOne = cards[0];
+cardsInPlay.push(cards[0]); 
+console.log(cardsInPlay);
+var cardTwo = cards[2];
+cardsInPlay.push(cards[2]);
+console.log(cardsInPlay);
+if ( cardsInPlay.length === 2 && 
+	cardsInPlay[0] === cardsInPlay[1])
+	{alert("You found a match!");
+} else {alert("Sorry,try again.");}
+
+
+
