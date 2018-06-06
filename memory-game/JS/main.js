b1b493f903582b87d53cb8381b937c58f5325426
Owner <owner@Owners-Air.gateway>
2018-06-06 21:46:08 +1000

    create game logic

--- memory-game/JS/main.js
+++ memory-game/JS/main.js
@@ -1,17 +1,17 @@
-console.log("Up and running!");
+
 var cards = ['queen','queen','king','king'];
 var cardsInPlay=[];
-var cardOne = cards[0];
-console.log("User flipped " + cardOne);
-cardsInPlay.push(cardOne); 
-console.log(cardsInPlay);
-var cardTwo = cards[2];
-console.log("User flipped " + cardTwo);
-cardsInPlay.push(cardTwo);
-console.log(cardsInPlay);
-if ( cardsInPlay.length === 2 && 
-	cardsInPlay[0] === cardsInPlay[1])
+var checkForMatch = function (){
+ if (cardsInPlay[0] === cardsInPlay[1])
 	{alert("You found a match!");
-} else {alert("Sorry,try again.");}
-
+} 
+else {
+alert("Sorry,try again.");}}
+var flipCard = function(cardId){
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+}
+flipCard(0);
+flipCard(2);
+checkForMatch();
 
